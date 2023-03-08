ef32cb00c46e2d6630e049cd5085130f973cec9f
Joe Chen <jc@unknwon.io>
2023-03-08 19:04:08 +0800

    frontend: show loading spinner on submit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -32,9 +32,12 @@ function LotteryForm({
   const [url, setURL] = useState("");
   const [winnerCount, setWinnerCount] = useState(1);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    setLoading(true);
 
     let backendUrl = "";
     if (process.env.NODE_ENV === "development") {
@@ -47,6 +50,7 @@ function LotteryForm({
       })
       .then((resp) => {
         setWinners(resp.data.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error.response);
@@ -125,12 +129,42 @@ function LotteryForm({
 
       <div className="pt-5">
         <div className="flex justify-end">
-          <button
-            type="submit"
-            className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            开始抽奖
-          </button>
+          {loading ? (
+            <button
+              type="submit"
+              className="disabled:opacity-75 disabled:cursor-not-allowed ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              disabled
+            >
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              正在抽奖...
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className="disabled:opacity-75 disabled:cursor-not-allowed ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              开始抽奖
+            </button>
+          )}
         </div>
       </div>
     </form>
